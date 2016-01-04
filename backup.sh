ffe0052e1406d10b784954e4577489bf9174f019
Timesqueezer <matzradloff@gmail.com>
2016-01-04 02:26:50 +0100

    Finished ansible and added backup script.

--- backup.sh
+++ backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ssh mooddiary.org pg_dumpall -U postgres | xz > /mnt/backup/pgdump-$(date --iso-8601=minutes).sql.xz
-rm -f /mnt/backup/backup.tar.gz
-tar czf /mnt/backup/backup.tar.gz \
+ssh mooddiary.org pg_dumpall -U postgres | xz > /media/sf_backup/pgdump-$(date --iso-8601=hours).sql.xz
+ssh root@mooddiary.org tar czf /home/backup.tar.gz \
     --ignore-failed-read \
     --exclude /var/cache \
     /boot /etc /home /opt /root /srv /var
+rm -f /media/sf_backup/backup.tar.gz
+scp mooddiary.org:backup.tar.gz /media/sf_backup/backup.tar.gz
