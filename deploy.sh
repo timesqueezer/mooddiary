8633dbbe566042784f03490e4455f81757968bc8
Timesqueezer <matzradloff@gmail.com>
2016-01-08 03:24:49 +0100

    Modified deploy script.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 cd /srv/mooddiary
 git pull
 echo "Copying vassal script"
+env/bin/python manage.py assets build
 sudo cp uwsgi_config.ini /etc/uwsgi/vassals/mooddiary.ini
 sudo chown http:http /etc/uwsgi/vassals/mooddiary.ini
 sudo chmod 640 /etc/uwsgi/vassals/mooddiary.ini
