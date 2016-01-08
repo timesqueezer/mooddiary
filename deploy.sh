216c5e55fea2736950a5e543470dbeea1d6c1603
Timesqueezer <matzradloff@gmail.com>
2016-01-08 04:08:19 +0100

    Removed piwik and added deploy and provision roles.

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-cd /srv/mooddiary
-git pull
-echo "Copying vassal script"
-env/bin/python manage.py assets build
-sudo cp uwsgi_config.ini /etc/uwsgi/vassals/mooddiary.ini
-sudo chown http:http /etc/uwsgi/vassals/mooddiary.ini
-sudo chmod 640 /etc/uwsgi/vassals/mooddiary.ini
